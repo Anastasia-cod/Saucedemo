7976575a038e9bde6b22cb6eafe31455f2984b64
admin <nastyabonybelay@gmail.com>
2023-05-18 20:43:45 +0300

    fix browser and userBuilder

--- SauceDemo/Builder/UserBuilder.cs
+++ SauceDemo/Builder/UserBuilder.cs
@@ -11,8 +11,6 @@ namespace SauceDemo.Builder
 
         public static User StandartUser => new User
         {
-            //Name = "standard_user",
-            //Password = "secret_sauce",
             Name = TestContext.Parameters.Get("StandartUserName"),
             Password = TestContext.Parameters.Get("StarndartUserPassword"),
             FirstName = Faker.Internet.UserName(lastName:""),
@@ -22,8 +20,6 @@ namespace SauceDemo.Builder
 
         public static User StandartUserWithIncorrectPassword => new User
         {
-            //Name = "standard_user",
-            //Password = "secret_sauce1",
             Name = TestContext.Parameters.Get("StandartUserName"),
             Password = TestContext.Parameters.Get("IncorrectPassword"),
         };
